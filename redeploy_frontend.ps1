8e9e41597ceabecfe9dba9d94009ce62cd9c92db
chasemuss <chasemuss@gmail.com>
2026-02-27 14:33:56 -0600

    Set environment variables for frontend deployment

--- redeploy_frontend.ps1
+++ redeploy_frontend.ps1
@@ -1,1 +1,5 @@
+$env:LAMBDA_URL="https://2xe1xk5eqd.execute-api.us-east-1.amazonaws.com/"
+$env:COGNITO_POOL="us-east-1_vbhlUbaJy"
+$env:COGNITO_CLIENT="28850uvbv4cnebnf4fm09je5t1"
+$env:AWS_REGION="us-east-1"
 .\scripts\deploy_frontend.ps1 -Bucket photoshare-frontend-b8f5869a -DistId E3IHXHSTRXFV1X
